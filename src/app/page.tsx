4358bcaf72465478f6f4f0b693ffe0defa268297
anshmehta7x <anshkapilmehta@gmail.com>
2025-01-04 10:05:39 +0530

    fix: restructure to not include navbar on create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import { Hero } from "@/components/hero/page";
+import Navbar from "@/components/ui/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Navbar />
       <div className="w-screen h-fit ">
-        <Image src={"./logo_svg.svg"} alt="" width={300} height={300} className="w-full"/>
+        <Image
+          src={"./logo_svg.svg"}
+          alt=""
+          width={300}
+          height={300}
+          className="w-full"
+        />
       </div>
     </main>
   );
