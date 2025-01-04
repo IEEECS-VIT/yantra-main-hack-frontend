b4141da800f82a4250174268c46be3a1a4aa3266
anshmehta7x <anshkapilmehta@gmail.com>
2025-01-04 22:17:14 +0530

    feat: stage 3 page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,17 +2,17 @@
 
 import { useState } from "react";
 import Stage1 from "./stage1";
+import Stage3 from "./stage3";
 import Stage2 from "./stage2";
 
 export default function Create() {
-  const [stage, setStage] = useState(2); // 1-4
+  const [stage, setStage] = useState(3); // 1-4
 
   return (
     <main className="flex flex-col items-center min-h-screen bg-custom-gradient">
       {stage === 1 && <Stage1 setStage={setStage} />}
       {stage === 2 && <Stage2 setStage={setStage} />}
-      {/* {state === 3 && ()}
-        {state === 4 && ()} */}
+      {stage === 3 && <Stage3 setStage={setStage} />}
     </main>
   );
 }
---
+++ src/app/create/stage3.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Heading from "@/components/creation/Heading";
+import Progressbar from "@/components/creation/Progressbar";
+import LayeredButton from "@/components/ui/orangeButton";
+import InputBox from "@/components/creation/InputBox";
+
+export default function Stage3({ setStage, teamSize = 4 }) {
+  const [memberNums, setmemberNums] = useState(
+    Array.from({ length: teamSize }, (_, index) => ({
+      [index + 1]: "",
+    })).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+  );
+
+  const handleNameChange = (position: number) => (value: string) => {
+    setmemberNums((prev) => ({
+      ...prev,
+      [position]: value || "", // Ensure value is never undefined
+    }));
+  };
+
+  return (
+    <div className="my-10 px-6">
+      <Heading text="CREATE YOUR TEAM" />
+      <Progressbar currentStep={3} />
+
+      <ol className="relative border-s border-white mt-10">
+        {Array.from({ length: teamSize }, (_, i) => i + 1).map(
+          (position, index) => (
+            <li
+              key={position}
+              className={`mb-10 ms-6 ${index === teamSize - 1 ? "border-0" : ""}`}
+            >
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-main-orange rounded-full -start-3 ">
+                <span className="text-white text-sm font-bold">{position}</span>
+              </span>
+              <div>
+                <InputBox
+                  placeholder={`Enter Reg Num`}
+                  value={memberNums[position] || ""}
+                  onChange={handleNameChange(position)}
+                />
+              </div>
+            </li>
+          )
+        )}
+      </ol>
+
+      {/* Continue Button */}
+      <div className="flex justify-center mt-16 md:mt-10">
+        <div className="flex justify-center items-center space-x-2 w-[60vw] md:w-[15vw]">
+          <LayeredButton text="SUBMIT" />
+        </div>
+      </div>
+    </div>
+  );
+}
