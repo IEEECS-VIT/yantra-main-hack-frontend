d7abb4a67a9c0263f2cddeea4d09070612ee5f78
Aniruddha Neema <aniruddhaneema@gmail.com>
2025-01-05 17:43:02 +0530

    fix: footer

--- src/components/ui/footer/FooterCard.tsx
+++ src/components/ui/footer/FooterCard.tsx
@@ -6,19 +6,19 @@ import Image from "next/image";
 import vitLogo from "../../../../public/logos/vit.svg";
 import yantraLogo from "../../../../public/logos/yantra.svg";
 import ieeeLogo from "../../../../public/logos/ieee-cs.svg";
+import { FaInstagram, FaTwitter, FaYoutube, FaFacebook,FaLinkedin } from 'react-icons/fa'; // Import social media icons
+
 
 const FooterCard = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <>
-      <footer className="relative p-8 bg-transparent rounded-xl">
+      <footer className="relative p-8 bg-transparent rounded-xl bottom-0">
         <div className="absolute inset-0 bg-white/10 backdrop-blur-md rounded-xl rounded-b-none" />
         
         <div className="relative max-w-6xl mx-auto md:grid flex flex-col-reverse md:grid-cols-3 gap-8 text-white">
           <div className="space-y-4 md:text-left text-center">
-            {/* <Text text="YANTRA" animation={false} />
-            <Text text="HACK" animation={false} /> */}
             <h1 className="text-xl md:text-5xl">YANTRA <span className="block">HACK</span></h1>
             <div className="space-y-2">
               <h2 className="text-lg md:text-xl italic">Made with <Heart className="inline text-white" /> by</h2>
@@ -33,32 +33,39 @@ const FooterCard = () => {
               <h2 className="md:text-2xl text-xl font-semibold text-[#557cff]">LINKS</h2>
               <div className="flex flex-col space-y-3 md:text-lg text-sm">
                 <Link 
-                  href="/" 
+                  href="https://www.instagram.com/sw_vit?igsh=MWgyMWdicDJ3aHphZQ== " 
                   target="_blank"
                   className="flex items-center gap-2 hover:text-blue-200 transition-colors"
                 >
-                  <span>Instagram</span>
+                  <FaInstagram className="text-xl" /> <span>Instagram</span>
                 </Link>
                 <Link 
-                  href="/" 
+                  href="https://x.com/VIT_univ" 
                   target="_blank"
                   className="flex items-center gap-2 hover:text-blue-200 transition-colors"
                 >
-                  <span>Twitter</span>
+                  <FaTwitter className="text-xl" /> <span>Twitter</span>
                 </Link>
                 <Link 
-                  href="/" 
+                  href="https://www.youtube.com/channel/UCA9pirjKrKlg2bCvPKRDkyg" 
                   target="_blank"
                   className="flex items-center gap-2 hover:text-blue-200 transition-colors"
                 >
-                  <span>Youtube</span>
+                  <FaYoutube className="text-xl" /> <span>Youtube</span>
                 </Link>
                 <Link 
-                  href="/" 
+                  href="https://www.facebook.com/vituniversity"
                   target="_blank"
                   className="flex items-center gap-2 hover:text-blue-200 transition-colors"
                 >
-                  <span>LinkedIn</span>
+                  <FaFacebook className="text-xl" /> <span>Facebook</span>
+                </Link>
+                <Link 
+                  href="https://www.linkedin.com/school/vellore-institute-of-technology/posts/?feedView=all"
+                  target="_blank"
+                  className="flex items-center gap-2 hover:text-blue-200 transition-colors"
+                >
+                  <FaLinkedin className="text-xl" /> <span>Linkedin</span>
                 </Link>
               </div>
             </div>
@@ -81,7 +88,7 @@ const FooterCard = () => {
                 >
                   director.sw@vit.ac.in
                 </Link>
-                <p>MB 210</p>
+                <p>MB-102</p>
               </div>
             </div>
           </div>
@@ -89,14 +96,13 @@ const FooterCard = () => {
 
       </footer>
       <div className="flex justify-evenly space-x-4 py-6 px-2 backdrop-blur-lg bg-opacity-90 bg-black/30">
-      <Image src={ieeeLogo} alt="IEEE CS" width={200} height={200} className="md:w-48 w-24"/>
-  <Image src={vitLogo} alt="VIT" width={200} height={200} className="md:w-48 w-24"/>
-  <Image src={yantraLogo} alt="Yantra" width={200} height={200} className="md:w-48 w-24"/>
-  
-</div>
-
+      <Image src={ieeeLogo} alt="VIT" width={200} height={200} className="md:w-48 w-24" />
+        <Image src={vitLogo} alt="VIT" width={200} height={200} className="md:w-48 w-24" />
+        <Image src={yantraLogo} alt="Yantra" width={200} height={200} className="md:w-48 w-24" />
+      </div>
     </>
   );
 };
 
 export default FooterCard;
+
