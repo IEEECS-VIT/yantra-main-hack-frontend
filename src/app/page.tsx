f54768ae58d7e4c654f641f78a7f8a7797ce413b
anshmehta7x <anshkapilmehta@gmail.com>
2025-01-25 00:24:08 +0530

    Add DialogOnLoad component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,13 @@ import Prizes from "@/components/ui/Prizes";
 import Footer from "@/components/ui/footer/footer";
 import FAQ from "@/components/faq";
 import { GoogleTagManager } from "@next/third-parties/google";
+import DialogOnLoad from "@/components/DialogOnLoad";
 
 export default function Home() {
   return (
     <main className="">
       <GoogleTagManager gtmId={"GTM-P55KCKBH"} />
+      <DialogOnLoad />
       <Hero />
       <Navbar />
       <div className="w-screen h-fit ">
